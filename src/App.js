01e2259bcba23f26279bafa3856f4c40f9d14314
chuxmykel <ngwobiachukwudi@gmail.com>
2019-07-07 14:36:17 +0100

    feat(footer): design footer component
    - import font awesome cdn for social media links
    - create footer component
    - style component
    
    [Delivers #167120611]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Layout/Header/Header';
+import Footer from './components/Layout/Footer/Footer';
 import './app.css';
 
 class App extends Component {
   render = () => (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
