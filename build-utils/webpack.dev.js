8d2d23d47092bfd1f7a1c8c805a80324c37507c8
chuxmykel <ngwobiachukwudi@gmail.com>
2019-07-20 00:23:46 +0100

    fix(failing build): fix build config files
    - change env variable accesing methods for prod and dev envs
    
    [Delivers #167401391]

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,13 +1,16 @@
 const { DefinePlugin } = require('webpack');
+const { config } = require('dotenv');
+
+const env = config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
-    new DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
+    new DefinePlugin(envKeys),
   ],
 };
